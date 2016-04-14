68238af4de2a232768f0b26d5d3efd8f1f3d35a3
James Volmert <james.volmert@gmail.com>
2016-04-14 10:39:32 -0600

    worked on production ready coding

--- src/_config.js
+++ src/_config.js
@@ -1,8 +1,7 @@
 var config = {};
-
 config.mongoURI = {
+    test: 'mongodb://localhost/first-mean-app-testing',
     development: 'mongodb://localhost/first-mean-app',
-    test: 'mongodb://localhost/first-mean-app-testing'
+    devolopment: process.env.mongoURI
 };
-
 module.exports = config;
